c75dacb96072d98386918b4786311e1356f4ed1b
nhut321 <znhut12z@gmail.com>
2022-01-22 16:28:31 +0700

    edit stats

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ const tabItem = $$('.tab-ui__item')
 
 window.onload = () => {
 	$('.spinner-box').style.display = 'block'
-	const tabItemFirst = $('.tab-ui__item')
+	const tabItemFirst = $$('.tab-ui__item')[1]
 	$('.line-bottom').style.left = tabItemFirst.offsetLeft + 'px'
 	$('.line-bottom').style.width = tabItemFirst.offsetWidth + 'px'
 }
 
 
-tabItem.forEach(v => {
-	v.onclick = (e) => {
-		tabItem.forEach(item => item.classList.remove('active'))
-		v.classList.add('active')
+tabItem.forEach((v,i) => {
+	v.onclick = function() {
+		const tabContent = $$('.tab-ui__content-item')[i]
+		$('.tab-ui__item.active').classList.remove('active')
+		$('.tab-ui__content-item.active').classList.remove('active')
+
+		tabContent.classList.add('active')
+		this.classList.add('active')
+
 		$('.line-bottom').style.width = v.offsetWidth + 'px'
 		$('.line-bottom').style.left = v.offsetLeft + 'px'
 	}
@@ -26,6 +31,27 @@ tabItem.forEach(v => {
 const urlPremier = `https://apiv3.apifootball.com/?action=get_standings&APIkey=d0f3c828602d68ebd214dbc3bbc3c9919af983b4cac330647728ab4df08db11f&league_id=152`
 const tBody = $('.table-standings tbody')
 const selectLeagueValue = $$('.select-league option')
+
+fetch('https://apiv3.apifootball.com/?action=get_topscorers&league_id=152&APIkey=d0f3c828602d68ebd214dbc3bbc3c9919af983b4cac330647728ab4df08db11f')
+	.then(res => res.json())
+	.then(data => {
+		let html = ''
+		data.forEach((v,i) => {
+			return html += `
+			<tr class='m-1'>
+				<td class='tbody-standing'>
+					${i+1}
+					<img src="${v.team_badge}" alt="">
+					${v.player_name}
+				</td>
+				<td style='font-weight: bold'>${v.goals}</td>
+		  </tr>
+			`
+		})
+		$('.table-stats tbody').innerHTML = html
+
+	})
+
 
 fetch(urlPremier)
 	.then(res => res.json())
@@ -47,7 +73,6 @@ fetch(urlPremier)
 			      <td>${v.overall_league_GA}</td>
 			      <td style='font-weight: bold'>${v.overall_league_PTS}</td>
 				</tr>
-
 			`
 		})
 		$('.spinner-box').style.display = 'none'
